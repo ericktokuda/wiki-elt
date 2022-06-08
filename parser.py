f2606f9f974d2e26ac3c7bdb93280f4fcb72bdf8
Eric K. Tokuda <eric.tokuda@maths.ox.ac.uk>
2022-06-08 19:59:03 +0100

    Getting better

--- parser.py
+++ parser.py
@@ -19,8 +19,48 @@ import bz2
 import subprocess
 import mwxml, mwparserfromhell
 from mwparserfromhell import parse as mwparse
+import pandas as pd
 
 
+class Wikitype:
+    BROKEN    = -1
+    REGULAR   =  0
+    FILE      =  1
+    CATEGORY  =  2
+    SELF      =  3
+    EXTERNAL  =  4
+    INTERLANG =  5
+
+# LANG DEPENDANT
+filens = ['Image', 'Media', 'File']
+wikins = ['WP', 'Wikipedia', 'Project']
+talkns = ['WT', 'Project talk', 'Project talk']
+userns = ['User']
+
+##########################################################
+def classify_wikilinks(links):
+    """Classify wiki link"""
+    for l in links:
+        link = l[2:-2]
+
+        pipeidx = link.find('|') # Remove text of the link
+        if pipeidx > 0: link = link[:pipeidx]
+
+        if link[0] == '#': # Not considering loops at the moment
+            continue
+
+        if not ':' in link: # (Supposedly) a regular inner link
+            info(':{}'.format(link))
+            # Gonna do something
+        else:
+            tokens = link.split(':')
+            if tokens[0] in filens:
+                continue
+            elif parts
+            # Will need to check 'Image, Wikipedia, Wikibooks,
+
+        breakpoint()
+    
 ##########################################################
 def parse_links(dumpsdir, olinksdir):
     """Parse the links from the dumps"""
@@ -32,7 +72,12 @@ def parse_links(dumpsdir, olinksdir):
 
     stdout = 'data/toy02.xml'
 
+    langspath = './data/wikilangs.csv'
+    aux = pd.read_csv('./data/wikilangs.csv')
+    aux = aux.loc[aux.available == 'active']['abbr'].tolist()
+
     dump = mwxml.Dump.from_file(stdout)
+     #TODO: Parse header to get the namespaces for this language, givne the ns ids
 
     for page in dump:
         # rev = next((x for x in page), None)
@@ -48,12 +93,17 @@ def parse_links(dumpsdir, olinksdir):
         # If no <nowiki />, or <nowiki></nowiki> mwxml does not work properly
 
         for rev in page:
-            # pass
             print(rev.parent_id, rev.id, rev.text)
             x = mwparse(rev.text)
             print(x.filter_wikilinks())
+            wikilinks1 = x.filter_wikilinks()
+            extlinks1 = x.filter_external_links()
+
+            print(extlinks1)
             print('######################')
-            print(x.filter_external_links())
+            print(wikilinks1)
+            print('######################')
+            wikilinks2 = classify_wikilinks(wikilinks1)
             breakpoint()
     return
 
