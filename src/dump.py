8f8357a1967340568970d681dcfd859575d1264e
Eric K. Tokuda <eric.tokuda@maths.ox.ac.uk>
2022-06-16 12:06:04 +0100

    Remove unnecessary caller (main.py)

--- src/dump.py
+++ src/dump.py
@@ -363,3 +363,33 @@ def process_dumps(dumpsdir, nprocs, outdir):
         fargs = [ [p, outdir] for p in dumppaths ]
         with Pool(processes=nprocs) as pool:
             L = pool.starmap(process_dump_protected, fargs)
+
+##########################################################
+def main(dumpdir, nprocs, outdir):
+    """Short description"""
+    info(inspect.stack()[0][3] + '()')
+
+    olinksdir = pjoin(outdir, 'links') # Out links
+    # os.makedirs(olinksdir, exist_ok=True)
+
+    # download_dumps(lang, date, dumpsdir)
+    process_dumps(dumpdir, nprocs, olinksdir)
+
+
+##########################################################
+if __name__ == "__main__":
+    info(datetime.date.today())
+    t0 = time.time()
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--dumpdir', default='data/dumps/', help='Dumps directory')
+    parser.add_argument('--nprocs', default=1, type=int, help='Dumps directory')
+    parser.add_argument('--outdir', default='/tmp/out/', help='Output directory')
+    args = parser.parse_args()
+
+    os.makedirs(args.outdir, exist_ok=True)
+    readmepath = create_readme(sys.argv, args.outdir)
+
+    main(args.dumpdir, args.nprocs, args.outdir)
+
+    info('Elapsed time:{:.02f}s'.format(time.time()-t0))
+    info('Output generated in {}'.format(args.outdir))
